49d096a20197669cdb2a444ea3cb411e68f340a1
emillaudon <emil.laudn@gmail.com>
2025-12-23 16:31:37 +0100

    added productrepository

--- src/main/java/com/example/shopbackend/demo/product/Product.java
+++ src/main/java/com/example/shopbackend/demo/product/Product.java
@@ -14,8 +14,7 @@ public class Product {
     private int price;
     private int stock;
 
-    public Product(final Long id, final String name, final int price, final int stock) {
-        this.id = id;
+    public Product(final String name, final int price, final int stock) {
         this.name = name;
         this.price = price;
         this.stock = stock;
---
+++ src/main/java/com/example/shopbackend/demo/product/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.shopbackend.demo.product;
+
+import java.util.List;
+
+public class ProductService {
+    private final ProductRepository repository;
+
+    public ProductService(final ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    public Product create(final String name, int price, int stock) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name is required.");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("Price must be above 0.");
+        }
+        if (stock < 0) {
+            throw new IllegalArgumentException("Stock must be above 0.");
+        }
+
+        return repository.save(new Product(name, price, stock));
+    }
+}
