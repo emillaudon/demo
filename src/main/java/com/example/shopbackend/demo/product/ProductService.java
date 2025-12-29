ef9781bb0136a1d2fb4cc07614306629b6f9f972
emillaudon <emil.laudn@gmail.com>
2025-12-29 13:53:43 +0100

    code cleanup

--- src/main/java/com/example/shopbackend/demo/product/ProductService.java
+++ src/main/java/com/example/shopbackend/demo/product/ProductService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.example.shopbackend.demo.common.InvalidDateRangeException;
 import com.example.shopbackend.demo.common.InvalidPriceRangeException;
 import com.example.shopbackend.demo.common.NotFoundException;
 
